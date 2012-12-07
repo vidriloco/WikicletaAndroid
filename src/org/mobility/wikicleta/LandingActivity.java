eca24b9becc4fe32625f3ef934d1c3adec50c18c
Alejandro Cruz <vidriloco@gmail.com>
2012-12-06 18:04:40 -0600

    Landing view

---
+++ src/org/mobility/wikicleta/LandingActivity.java
@@ -0,0 +1,27 @@
+package org.mobility.wikicleta;
+
+import org.wikicleta.common.AppBase;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LandingActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AppBase.currentActivity = this;
+		this.setContentView(R.layout.activity_landing); 
+		
+		findViewById(R.id.join).setOnClickListener(
+			new View.OnClickListener() {
+				@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(AppBase.currentActivity, LoginActivity.class);
+				AppBase.currentActivity.startActivity(intent);
+			}
+		});
+	}
+}
---
+++ src/org/wikicleta/common/AppBase.java
@@ -0,0 +1,128 @@
+package org.wikicleta.common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Timer;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.mobility.wikicleta.R;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.view.Display;
+
+@SuppressLint("NewApi")
+public class AppBase {
+
+	private static String url = "http://132.248.51.251:9000/";
+	private static String routesResourceURL = "transports/8/lines";
+	private static String vehiclesResourceURL = "vehicles";
+	private static String instantsResourceURL = "instants/lastminute";
+	private static String serviceGeneralStatus = "service/status";
+	private static Timer timer;
+	
+	public static Activity currentActivity;
+	
+	public static Timer globalTimer() {
+		if(timer == null) {
+			timer = new Timer();
+		}
+		return timer;
+	}
+	
+	public static Display getScreenSize() {
+		return currentActivity.getWindowManager().getDefaultDisplay();
+	}
+	
+	public static String urlFor(String string) throws BadURLResourceException {
+		if(string.equalsIgnoreCase("routes"))
+			return url.concat(routesResourceURL);
+		else if(string.equalsIgnoreCase("vehicles"))
+			return url.concat(vehiclesResourceURL);
+		else if(string.equalsIgnoreCase("instants"))
+			return url.concat(instantsResourceURL);
+		else if(string.equalsIgnoreCase("serviceStatus"))
+			return url.concat(serviceGeneralStatus);
+		else
+			throw new AppBase.BadURLResourceException();
+	}
+	
+	private static String fetchResource(String resource) {
+		InputStream is = null;
+	    String result = new String();
+	    try{
+	         HttpClient httpclient = new DefaultHttpClient();
+	         HttpGet get = new HttpGet(urlFor(resource));
+	         HttpResponse response = httpclient.execute(get);
+	         HttpEntity entity = response.getEntity();
+	         is = entity.getContent();
+	     }catch(Exception e){ }
+
+	      try{
+	         BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"),8);
+	         StringBuilder sb = new StringBuilder();
+	         String line = null;
+	         while ((line = reader.readLine()) != null) {
+	             sb.append(line + "\n");
+	         }
+	         is.close();
+	         result=sb.toString();
+	         
+	     } catch(Exception e){}
+	     return result;
+	}
+	
+	public static JSONArray fetchResourceAsArray(String resource) {
+		String result = fetchResource(resource);
+	    JSONArray json = null;
+	    try{
+	    	 // Handle empty results arising from errors on network comunication
+	    	 if(result.isEmpty()) {
+	    		 if(resource.equalsIgnoreCase("routes"))
+	    			 result = readRawTextFile(R.raw.routes);
+	    	 }
+	         json = new JSONArray(result);
+	     }catch(JSONException e){}
+	     return json;
+	}
+	
+	public static String fetchResourceAsString(String resource) {
+		return fetchResource(resource).replaceAll("[^0-9]","");
+	}
+	
+	public static InputStream rawTextStream(int resId) {
+		return currentActivity.getResources().openRawResource(resId);
+	}
+	
+	public static String readRawTextFile(int resId)
+    {
+		InputStreamReader inputreader = new InputStreamReader(rawTextStream(resId));
+		BufferedReader buffreader = new BufferedReader(inputreader);
+		String line;
+		StringBuilder text = new StringBuilder();
+		try {
+			while (( line = buffreader.readLine()) != null) {
+                   text.append(line);
+                   text.append('\n');
+                 }
+           } catch (IOException e) {
+               return null;
+           }
+		return text.toString();
+    }
+	
+	public static class BadURLResourceException extends Exception
+	{
+
+		private static final long serialVersionUID = 1L;
+		
+	}
+}
