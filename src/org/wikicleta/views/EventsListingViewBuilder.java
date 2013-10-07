5f39215ef8b54ea3198059367569a590d9625b09
Alejandro Eduardo Cruz Paz <alex@wikicleta.com>
2013-10-07 13:23:26 -0500

    Issue #30: When no events are visible, show empty window message

--- src/org/wikicleta/views/EventsListingViewBuilder.java
+++ src/org/wikicleta/views/EventsListingViewBuilder.java
@@ -1,5 +1,6 @@
 package org.wikicleta.views;
 
+import org.interfaces.EventInterface;
 import org.interfaces.MarkerInterface;
 import org.wikicleta.R;
 import org.wikicleta.activities.EventsActivity;
@@ -16,24 +17,19 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class EventsListingViewBuilder {
 
-	public static void buildView(final EventsActivity activity) {
-
-        AlertDialog.Builder dialog = new AlertDialog.Builder(activity);
+	public static void buildEmptyView(final EventsActivity activity) {
+		AlertDialog.Builder dialog = new AlertDialog.Builder(activity);
         LayoutInflater inflater = activity.getLayoutInflater();
-        final View view = inflater.inflate(R.layout.dialog_events_list, null);
+        final View view = inflater.inflate(R.layout.dialog_events_list_empty, null);
         
         TextView dialogTitle = (TextView) view.findViewById(R.id.dialog_menu_title);
         dialogTitle.setTypeface(AppBase.getTypefaceStrong());
-        
-        final ListView listview = (ListView) view.findViewById(R.id.list_events);
-        final EventsListAdapter listAdapter = new EventsListAdapter(activity, activity.visibleMarkers());
-	    listview.setAdapter(listAdapter);
-	    listview.getCheckedItemPositions();
-	    listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        
         dialog.setView(view);
-        final AlertDialog visibleDialog = dialog.create();
         
+        TextView emptyMessageText = (TextView) view.findViewById(R.id.empty_message_text);
+        emptyMessageText.setTypeface(AppBase.getTypefaceStrong());
+        
+        final AlertDialog visibleDialog = dialog.create();
         view.findViewById(R.id.dialog_close).setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -43,17 +39,54 @@ public class EventsListingViewBuilder {
         	
         });
         
-	    listview.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> adapterParent, View view, int position, long id) {
-				visibleDialog.dismiss();
-				activity.centerOnEvent((MarkerInterface) listAdapter.getItem(position));
-			}
-
-	    });
-        
         visibleDialog.show();
+	}
+	
+	public static void buildView(final EventsActivity activity) {
+		EventInterface [] events = activity.visibleMarkers();
+		
+		if(events.length == 0) {
+			buildEmptyView(activity);
+		} else {
+			AlertDialog.Builder dialog = new AlertDialog.Builder(activity);
+	        LayoutInflater inflater = activity.getLayoutInflater();
+	        final View view = inflater.inflate(R.layout.dialog_events_list, null);
+	        
+	        TextView dialogTitle = (TextView) view.findViewById(R.id.dialog_menu_title);
+	        dialogTitle.setTypeface(AppBase.getTypefaceStrong());
+	        
+	        final ListView listview = (ListView) view.findViewById(R.id.list_events);
+	        final EventsListAdapter listAdapter = new EventsListAdapter(activity, activity.visibleMarkers());
+		    listview.setAdapter(listAdapter);
+		    listview.getCheckedItemPositions();
+		    listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+	        
+	        dialog.setView(view);
+	        final AlertDialog visibleDialog = dialog.create();
+	        
+	        view.findViewById(R.id.dialog_close).setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					visibleDialog.dismiss();
+				}
+	        	
+	        });
+	        
+		    listview.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> adapterParent, View view, int position, long id) {
+					visibleDialog.dismiss();
+					activity.centerOnEvent((MarkerInterface) listAdapter.getItem(position));
+				}
+
+		    });
+	        
+	        visibleDialog.show();
+		}
+		
+        
 	}
 
 }
